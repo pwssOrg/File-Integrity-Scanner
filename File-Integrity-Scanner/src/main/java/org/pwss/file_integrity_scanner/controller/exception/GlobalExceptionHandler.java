16a0bc08744a0ef894063e6d3b096f542b20bc4f
pwgit-create <snow_900@outlook.com>
2025-08-25 01:35:08 +0200

    - Addition of Swagger annotations for each endpoint in the FileIntegrityController - Changed status code when a scan is started and no active directories are found: `Conflict` -> `Precondition Failed` - Added if block condition and a response body for Access denied in the GlobalExceptionHandler

--- File-Integrity-Scanner/src/main/java/org/pwss/file_integrity_scanner/controller/exception/GlobalExceptionHandler.java
+++ File-Integrity-Scanner/src/main/java/org/pwss/file_integrity_scanner/controller/exception/GlobalExceptionHandler.java
@@ -2,20 +2,21 @@ package org.pwss.file_integrity_scanner.controller.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    
   org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        log.error("Error message - {}", ex.getMessage());
-        return new ResponseEntity<>("An error occurred when calling an endpoint: ",HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception ex) {
+    log.error("Error message - {}", ex.getMessage());
+    if (ex instanceof AccessDeniedException)
+      return new ResponseEntity<>("Access denied", HttpStatus.UNAUTHORIZED);
+    else
+      return new ResponseEntity<>("An error occurred when calling an endpoint: ", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
